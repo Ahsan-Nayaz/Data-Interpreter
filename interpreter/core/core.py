815c9c95411566a9be545a36821a387a914ce2b5
ahsan_admin <ahsan.nayaz@blenheimchalcot.com>
2023-11-06 12:46:53 +0530

    Revert "Refactor core.py: Update error logging in app.py, and Improve datetime handling instructions in system_message.txt"
    This reverts commit 9487bea2f21d1c0929dd4e9c5a4c9a9459219a92.

--- interpreter/core/core.py
+++ interpreter/core/core.py
@@ -80,7 +80,7 @@ class Interpreter:
         config = get_config(config_path)
         self.__dict__.update(config)
 
-    def chat(self, message=None, display=True, stream=False, uuid=None):
+    async def chat(self, message=None, display=True, stream=False, uuid=None):
         if stream:
             return self._streaming_chat(message=message, display=display, uuid=uuid)
         
