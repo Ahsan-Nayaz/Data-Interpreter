f91afdcbe642606ad8e22ae2512b08f009ce5b25
ahsan_admin <ahsan.nayaz@blenheimchalcot.com>
2023-11-22 11:52:01 +0530

    Refactor __init__.py and app.py: Add create_interpreter factory function, replace direct Interpreter instantiation with factory function call in app.py

--- interpreter/__init__.py
+++ interpreter/__init__.py
@@ -1,17 +1,39 @@
-from .core.core import Interpreter
 import sys
+from .core.core import Interpreter
+from .cli.cli import cli
 
-# This is done so when users `import interpreter`,
-# they get an instance of interpreter:
 
-sys.modules["interpreter"] = Interpreter()
+def create_interpreter(**kwargs):
+    """
+    Factory function to create an instance of Interpreter with the provided keyword arguments.
 
-# **This is a controversial thing to do,**
-# because perhaps modules ought to behave like modules.
+    Parameters:
+        **kwargs: Keyword arguments to be set as attributes in the Interpreter instance.
 
-# But I think it saves a step, removes friction, and looks good.
+    Returns:
+        An instance of Interpreter initialized with the provided arguments.
+    """
+    # Create a new interpreter instance
+    new_interpreter = Interpreter()
 
-#     ____                      ____      __                            __           
+    # Iterate through the provided keyword arguments
+    for key, value in kwargs.items():
+        # Check if the attribute exists in the interpreter
+        if hasattr(new_interpreter, key):
+            # Check if the provided value is of the correct type
+            if isinstance(value, type(getattr(new_interpreter, key))):
+                setattr(new_interpreter, key, value)
+            else:
+                print(
+                    f"Type mismatch: '{key}' should be of type {type(getattr(new_interpreter, key))}. Using the default value instead.")
+
+        else:
+            print(
+                f"Unknown attribute: '{key}'. Ignoring.")
+
+    return new_interpreter
+
+#     ____                      ____      __                            __
 #    / __ \____  ___  ____     /  _/___  / /____  _________  ________  / /____  _____
 #   / / / / __ \/ _ \/ __ \    / // __ \/ __/ _ \/ ___/ __ \/ ___/ _ \/ __/ _ \/ ___/
 #  / /_/ / /_/ /  __/ / / /  _/ // / / / /_/  __/ /  / /_/ / /  /  __/ /_/  __/ /    
