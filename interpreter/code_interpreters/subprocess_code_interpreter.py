c014a73a8c6d9f5afd7931ad3c807d761c6c3fcd
ahsan_admin <ahsan.nayaz@blenheimchalcot.com>
2023-11-28 13:44:43 +0530

    Test

--- interpreter/code_interpreters/subprocess_code_interpreter.py
+++ interpreter/code_interpreters/subprocess_code_interpreter.py
@@ -148,9 +148,6 @@ class SubprocessCodeInterpreter(BaseCodeInterpreter):
                 if retry_count > max_retries:
                     yield {"output": "Maximum retries reached. Could not execute code."}
                     return
-            finally:
-                # Close stdin to signal the end of input
-                self.process.stdin.close()
         while True:
             if not self.output_queue.empty():
                 yield self.output_queue.get()
