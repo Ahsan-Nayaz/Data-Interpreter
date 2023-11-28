61c280f469c275fae5f212dbc741a15d73f43ac6
ahsan_admin <ahsan.nayaz@blenheimchalcot.com>
2023-11-28 12:39:25 +0530

    Test

--- interpreter/code_interpreters/subprocess_code_interpreter.py
+++ interpreter/code_interpreters/subprocess_code_interpreter.py
@@ -168,7 +168,7 @@ class SubprocessCodeInterpreter(BaseCodeInterpreter):
                     break
 
     def handle_stream_output(self, stream, is_error_stream):
-        for line in iter(stream.readline, ""):
+        for line in stream:
             # line = line.decode('utf-8')
             if self.debug_mode:
                 print(f"Received output line:\n{line}\n---")
